648a4c6072af726cf1763e9646c0e1695d09cc73
your_github_username <your_github_email>
2022-03-24 13:44:48 +0530

    WELCOME TO ADDRESS BOOK SERVICE JDBC

---
+++ src/main/java/com/bridgelabz/day35/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day35;
+
+public class AddressBookMain
+{
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ADDRESS BOOK SERVICE JDBC");
+    }
+}
